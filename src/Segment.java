11129a0feab861e07f5f170078ffd80afbb9a1b9
Ap4chee <gierchucknorbertinio@gmail.com>
2024-03-13 17:07:01 +0100

    3rd

---
+++ src/Segment.java
@@ -0,0 +1,9 @@
+public class Segment {
+    private Point startPoint;
+    private Point endPoint;
+
+    public void setStartPoint(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
